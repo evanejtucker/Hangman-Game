6c1eba7d28b85edff2aa9480c4a80741c605a97a
Evan Tucker <Evan Tucker>
2017-05-17 10:33:20 -0600

    version 1.0

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+var rulers = ["Nebuchadnezzar_II",
+			  "Augustus_Caesar", 
+			  "Xerxes_I", 
+			  "Genghis_Khan", 
+			  "Agamemnon", 
+			  "Charlemagne"]
+
+function name_generater() {
+}
+
+
