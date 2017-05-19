785141a7b0284bfe5a33fa4ee69c2b921472e312
Evan Tucker <Evan Tucker>
2017-05-19 09:39:13 -0600

    version 2.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,39 @@
+window.onload = function() {
+var alphabet = ["a", "b", "c", "d", "e", "f", 
+				"g", "h", "i", "j", "k", "l", 
+				"m", "n", "o", "p", "q", "r", 
+				"s", "t", "u", "v", "w", "z", 
+				"y", "z"]
+var rand_ruler; 
+var word;
 
 var rulers = ["Nebuchadnezzar_II",
 			  "Augustus_Caesar", 
 			  "Xerxes_I", 
 			  "Genghis_Khan", 
 			  "Agamemnon", 
-			  "Charlemagne"]
+			  "Charlemagne"];
 
-function name_generater() {
+
+
+// selects random ruler, which will be used as question.
+rand_ruler = rulers[Math.floor(Math.random() * rulers.length)];
+word = rand_ruler.replace((/\s/g, "-"));
+
+
+for (i=0; i<rand_ruler.length; i++) {
+	if (rand_ruler[i]==="_") {
+		console.log(" ");
+		word.innerHTML = "_";
+	}
+
+	else {
+		console.log("_");
+		word[i] = "_";
+	}
 }
+document.getElementById("question").innerHTML = word;
 
 
+
+}
