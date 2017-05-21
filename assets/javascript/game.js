ec50ecd8835add118d21cd8de4679225ee1e2d8a
Evan Tucker <Evan Tucker>
2017-05-21 17:23:27 -0600

    version 10.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -76,6 +76,9 @@ function checkLetters(letter) {
 	else {
 		wrongGuesses.push(letter);
 		guessesLeft--;
+		if (guessesLeft < 10) {
+			document.getElementById("guessesLeft").style.color = "red";
+		}
 	}
 
 	// testing / debugging
@@ -122,121 +125,147 @@ function roundComplete() {
 
  		document.getElementById("hangman-pic").style.height = "350px";
  		document.getElementById("hangman-pic").style.border = "solid 2px white";
+ 		document.getElementById("description").innerHTML = "Guess the letters correctly to form a word before your run out of guesses.";
+ 		document.getElementById("description").style.textAlign = "left";
+ 		document.getElementById("powers-tag").innerHTML = "Abilities:";
 
 		if(selectedWord === "batman") {
 			document.getElementById("hangman-pic").src = "assets/images/batman.jpg";
 			document.getElementById("nameTag").innerHTML = "Batman";
-			// document.getElementById("description").innerHTML = "";
+			document.getElementById("description").innerHTML = "exceptional martial artist, combat strategy, inexhaustible wealth, brilliant deductive skill, advanced technology";
 		}
 
 		else if(selectedWord === "superman") {
 			document.getElementById("hangman-pic").src = "assets/images/superman.jpg";
 			document.getElementById("nameTag").innerHTML = "Superman";
+			document.getElementById("description").innerHTML = "super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor";
 		}
 
 		else if(selectedWord === "aquaman") {
 			document.getElementById("hangman-pic").src = "assets/images/aquaman.jpg";
 			document.getElementById("nameTag").innerHTML = "Aquaman";
+			document.getElementById("description").innerHTML = "super strength, durability, control over sea life, exceptional swimming ability, ability to breathe underwater";
 		}
 
 		else if(selectedWord === "shazam") {
 			document.getElementById("hangman-pic").src = "assets/images/shazam.jpg";
 			document.getElementById("nameTag").innerHTML = "Shazam";
+			document.getElementById("description").innerHTML = "super strength, flight, invulnerability, super speed, superhuman hearing, healing factor, intelligence, magic";
 		}
 
 		else if(selectedWord === "cyborg") {
 			document.getElementById("hangman-pic").src = "assets/images/cyborg.jpg";
 			document.getElementById("nameTag").innerHTML = "Cyborg";
+			document.getElementById("description").innerHTML = "super strength, advanced technology, instant weaponry, genius-level intellect, control over technology, computer hacking, durability, teleportation";
 		}
 
 		else if(selectedWord === "wolverine") {
 			document.getElementById("hangman-pic").src = "assets/images/wolverine.jpg";
 			document.getElementById("nameTag").innerHTML = "Wolverine";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "daredevil") {
 			document.getElementById("hangman-pic").src = "assets/images/daredevil.jpg";
 			document.getElementById("nameTag").innerHTML = "Daredevil";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "firestorm") {
 			document.getElementById("hangman-pic").src = "assets/images/firestorm.jpg";
 			document.getElementById("nameTag").innerHTML = "Firestorm";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "colossus") {
 			document.getElementById("hangman-pic").src = "assets/images/colossus.jpg";
 			document.getElementById("nameTag").innerHTML = "Colossus";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "deadpool") {
 			document.getElementById("hangman-pic").src = "assets/images/deadpool.jpg";
 			document.getElementById("nameTag").innerHTML = "Deadpool";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "spiderman") {
 			document.getElementById("hangman-pic").src = "assets/images/spiderman.jpg";
 			document.getElementById("nameTag").innerHTML = "Spiderman";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "magneto") {
 			document.getElementById("hangman-pic").src = "assets/images/magneto.jpg";
 			document.getElementById("nameTag").innerHTML = "Magneto";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "juggernaut") {
 			document.getElementById("hangman-pic").src = "assets/images/juggernaut.jpg";
 			document.getElementById("nameTag").innerHTML = "Juggernaut";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "apocalypse") {
 			document.getElementById("hangman-pic").src = "assets/images/apocalypse.jpg";
 			document.getElementById("nameTag").innerHTML = "Apocalypse";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "doomsday") {
 			document.getElementById("hangman-pic").src = "assets/images/doomsday.jpg";
 			document.getElementById("nameTag").innerHTML = "Doomsday";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "scarecrow") {
 			document.getElementById("hangman-pic").src = "assets/images/scarecrow.jpg";
 			document.getElementById("nameTag").innerHTML = "Scarecrow";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "deathstroke") {
 			document.getElementById("hangman-pic").src = "assets/images/deathstroke.jpg";
 			document.getElementById("nameTag").innerHTML = "Deathstroke";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "ultron") {
 			document.getElementById("hangman-pic").src = "assets/images/ultron.jpg";
 			document.getElementById("nameTag").innerHTML = "Ultron";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "brainiac") {
 			document.getElementById("hangman-pic").src = "assets/images/brainiac.jpg";
 			document.getElementById("nameTag").innerHTML = "Brainiac";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "sinestro") {
 			document.getElementById("hangman-pic").src = "assets/images/sinestro.jpg";
 			document.getElementById("nameTag").innerHTML = "Sinestro";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "joker") {
 			document.getElementById("hangman-pic").src = "assets/images/joker.jpg";
 			document.getElementById("nameTag").innerHTML = "The Joker";
+			document.getElementById("description").innerHTML = "";
 		}
 
 		else if(selectedWord === "darkseid") {
 			document.getElementById("hangman-pic").src = "assets/images/darkseid.jpg";
 			document.getElementById("nameTag").innerHTML = "Darkseid";
+			document.getElementById("description").innerHTML = "";
 		}
 
 			else {
 				document.getElementById("hangman-pic").src = "assets/images/hangman.png";
 				document.getElementById("hangman-pic").style.height = "250px";
+			 	document.getElementById("description").innerHTML = "Guess the letters correctly to form a word before your run out of guesses.";
+	
 			}
 }
 
